86142b41fa49d13ad915fcdbe4a56c00a4882bbd
Sam Collard <gitlab@swifteagle.co.uk>
2020-07-30 14:37:04 +0100

    Updated for Forge 1.15.2, no other changes

--- src/main/java/com/alexanderstrada/practicaltools/items/GreataxeItem.java
+++ src/main/java/com/alexanderstrada/practicaltools/items/GreataxeItem.java
@@ -25,8 +25,8 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import java.util.ArrayList;
 import java.util.Set;
